961255e442396e80f78405580be5e1f916f8ed2d
zusoomro <zusoomro@seas.upenn.edu>
2020-02-27 14:10:21 -0500

    MainActivity updates

--- app/src/main/java/edu/upenn/cis350/hw4/MainActivity.java
+++ app/src/main/java/edu/upenn/cis350/hw4/MainActivity.java
@@ -1,5 +1,6 @@
 package edu.upenn.cis350.hw4;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -8,18 +9,52 @@ import com.google.android.material.snackbar.Snackbar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+
+    static String difficulty = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Handle the speener
+        Spinner speener = (Spinner) findViewById(R.id.spinner);
+
+        // Create an Array Adapter to pass the options into the Speener
+        ArrayAdapter<CharSequence> aa = ArrayAdapter.createFromResource(this,
+                 R.array.speener_values_array, android.R.layout.simple_spinner_item);
+        aa.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        speener.setAdapter(aa);
+
+        // Let the speener know that we handle its actions in this activity
+        speener.setOnItemSelectedListener(this);
+
     }
 
-    public void
+    // The spinner methods
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        difficulty = (String) parent.getItemAtPosition(position);
+        Log.d("Notice", difficulty);
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+    }
+
+    public void startQuiz(View view) {
+        Intent intent  = new Intent(this, QuizActivity.class);
+        intent.putExtra("difficulty", difficulty);
+        startActivity(intent);
+    }
 
 }
--- app/src/main/java/edu/upenn/cis350/hw4/QuizActivity.java
+++ app/src/main/java/edu/upenn/cis350/hw4/QuizActivity.java
@@ -1,7 +1,120 @@
 package edu.upenn.cis350.hw4;
 
-public class QuizActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.Random;
+
+public class QuizActivity extends AppCompatActivity {
+
+    static String difficulty = "";
+    Random r = new Random();
+
+    int topNumber;
+    char[] topCharArray;
+
+    int bottomNumber;
+    char[] bottomCharArray;
+
+    int answerNumber;
+    char[] answerCharArray;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quiz);
+
+        // Get the Intent that started this activity and extract the string
+        Intent intent = getIntent();
+        difficulty = intent.getStringExtra(MainActivity.difficulty);
 
 
+    }
+
+    private void generateNumbers(String difficulty) {
+        if (difficulty.equals("Easy")) {
+            // get the answer digits
+            answerCharArray[1] = (char) r.nextInt(9);
+            answerCharArray[0] = (char) r.nextInt(9);
+            answerNumber = Integer.parseInt(String.valueOf(answerCharArray));
+
+            // get the top number digits
+            topCharArray[1] = (char) r.nextInt((int) answerCharArray[1]);
+            topCharArray[0] = (char) r.nextInt((int) answerCharArray[0]);
+            topNumber = Integer.parseInt(String.valueOf(topCharArray));
+
+            // get the bottom number digits
+            bottomNumber = answerNumber - topNumber;
+            bottomCharArray = String.valueOf(bottomNumber).toCharArray();
+
+        } else if (difficulty.equals("Medium")) {
+            int overFlowDigit = r.nextInt(1);
+            int sumOfOverFlow = r.nextInt(8) + 10;
+            if (overFlowDigit == 1) {
+                topCharArray[1] = (char) r.nextInt(sumOfOverFlow);
+                bottomCharArray[1] = (char) (sumOfOverFlow - (int) topCharArray[1]);
+                topCharArray[1] = (char) (sumOfOverFlow - (int) bottomCharArray[1]);
+
+                topCharArray[0] = (char) r.nextInt(8);
+                bottomCharArray[0] = (char) (8 - (int) topCharArray[0]);
+                topCharArray[0] = (char) (topCharArray[0] - (int) bottomCharArray[0]);
+
+                topNumber = Integer.parseInt(String.valueOf(topCharArray));
+                bottomNumber =  Integer.parseInt(String.valueOf(bottomCharArray));
+
+                answerNumber = topNumber + bottomNumber;
+                answerCharArray = String.valueOf(answerNumber).toCharArray();
+
+            } else {
+
+                topCharArray[0] = (char) r.nextInt(sumOfOverFlow);
+                bottomCharArray[0] = (char) (sumOfOverFlow - (int) topCharArray[0]);
+                topCharArray[0] = (char) (sumOfOverFlow - (int) bottomCharArray[0]);
+
+                topCharArray[1] = (char) r.nextInt(9);
+                bottomCharArray[1] = (char) (8 - (int) topCharArray[1]);
+                topCharArray[1] = (char) (topCharArray[1] - (int) bottomCharArray[1]);
+
+                topNumber = Integer.parseInt(String.valueOf(topCharArray));
+                bottomNumber =  Integer.parseInt(String.valueOf(bottomCharArray));
+
+                answerNumber = topNumber + bottomNumber;
+                answerCharArray = String.valueOf(answerNumber).toCharArray();
+            }
+
+        } else if (difficulty.equals("Hard")) {
+
+            // the first digits can add up to 18
+            int entionallyWaitingForTheLastDayToDoMyHomework = r.nextInt(8) + 10;
+
+            topCharArray[1] = (char) r.nextInt(entionallyWaitingForTheLastDayToDoMyHomework);
+            entionallyWaitingForTheLastDayToDoMyHomework -= (int) topCharArray[1];
+            bottomCharArray[1] = (char) entionallyWaitingForTheLastDayToDoMyHomework;
+
+            // the second digits can add up to 18, with the one maybe carried over from before
+            entionallyWaitingForTheLastDayToDoMyHomework = r.nextInt(8) + 10;
+
+            topCharArray[0] = (char) r.nextInt(entionallyWaitingForTheLastDayToDoMyHomework);
+            entionallyWaitingForTheLastDayToDoMyHomework -= (int) topCharArray[0];
+            bottomCharArray[0] = (char) entionallyWaitingForTheLastDayToDoMyHomework;
+
+            // put together the numbers and the answer
+            topNumber = Integer.parseInt(String.valueOf(topCharArray));
+            bottomNumber = Integer.parseInt(String.valueOf(bottomCharArray));
+            answerNumber = topNumber + bottomNumber;
+
+        } else {
+            Log.d("ERROR", "Couldn't parse difficulty");
+        }
+    }
+
+    public void handleButtonClick(View view, String difficulty) {
+
+    }
 
 }
